a550768d6da0abff05480082ff6df31b41167843
Philipp Scheit <p.scheit@ps-webforge.com>
2016-05-09 11:39:55 +0200

    add a cache for paths

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ module.exports = function(gulp, config, rootRequire) {
   this.configuredTasks = require('./configured-tasks');
   this.gulpTasks = require('./gulp-tasks');
   this.require = rootRequire;
+  this.resolvedModules = {};
 
   if (!that.require) {
     throw new Error('Please pass the require function from the gulpfile.js to the builder as third Argument');
@@ -104,20 +105,43 @@ module.exports = function(gulp, config, rootRequire) {
   this.resolveModule = function(name, alternative) {
     var path = require('path');
 
+    if (that.resolvedModules.hasOwnProperty(name)) {
+      return that.resolvedModules[name];
+    }
+
+    if (that.resolvedModules.hasOwnProperty(alternative)) {
+      return that.resolvedModules[alternative];
+    }
+
     // this does not have a "main" defined, so that it cannot be required()
     if (name === 'font-awesome') {
-      var exec = require('child_process').execSync;
-      var lspath = exec('npm ls '+name+' --parseable', { cwd: that.config.root});
-      return lspath.toString().replace(/[\r\n]*$/g, '');
+      var execSync = require('child_process').execSync;
+      var lspath;
+      try {
+        lspath = execSync('npm ls '+name+' --parseable', { cwd: that.config.root});
+      } catch (e) {
+        if (e.status === 1) {
+          lspath = e.stdout;
+        } else {
+          throw e;
+        }
+      }
+      // take the first line
+      var match = lspath.toString().match(/^(.*?)([\r\n]+|$)/);
+      if (match === null) {
+        throw new Error('Could not parse ls output: '+lspath.toString());
+      }
+      
+      return that.resolvedModules[name] = match[1];
     }
 
     try {
-      return path.dirname(that.require.resolve(name));
+      return that.resolvedModules[name] = path.dirname(that.require.resolve(name));
     } catch (exc) {
 
       if (alternative) {
         try {
-          return path.dirname(that.require.resolve(alternative));
+          return that.resolvedModules[alternative] = path.dirname(that.require.resolve(alternative));
         } catch (exc2) {
         }
       }
